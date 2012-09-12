98575aff3b6096e30f5ecf701319605e3f07784d
Guillaume Espanel <yliugaome@gmail.com>
2012-09-12 11:18:34 +0200

    Ajout boucles for

---
+++ tp_boucle_for_fichier.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+
+for iterateur in *
+do
+    echo "un fichier!!! $iterateur"
+done
---
+++ tp_boucle_for_list.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+for iterateur in salut bonjour ouais youpi
+do
+    echo "un élément de la liste!!! $iterateur"
+done
+echo "Hors de la boucle : $iterateur"
